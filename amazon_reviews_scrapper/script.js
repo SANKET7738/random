a8da190323d115e071a408860caaf5d9b4400e3e
SANKET MHATRE <sanket.mhatre@vit.edu.in>
2020-09-26 13:01:51 +0530

    updated amazon_reviews_scrapper

--- amazon_reviews_scrapper/script.js
+++ amazon_reviews_scrapper/script.js
@@ -1,15 +1,19 @@
 const cheerio = require('cheerio');
 const got = require('got');
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-//provide the URL of the product's page
-const url = 'https://www.amazon.in/Apple-iPhone-11-64GB-Black/dp/B07XVMDRZY/ref=sr_1_1?dchild=1&keywords=iphone&qid=1600354773&s=electronics&sr=1-1';
-
-got(url).then(response => {
-    const $ = cheerio.load(response.body);
-    // Load the reviews
-    const reviews = $('.review');
-    reviews.each((i, review) => {
-        const textReview = $(review).find('.review-text').text();
-        console.log(textReview);
+readline.question('Provide the URL of the Amazon Prdouct you want to scrape. \n', url => {
+    got(url).then(response => {
+        const $ = cheerio.load(response.body);
+        // Load the reviews
+        const reviews = $('.review');
+        reviews.each((i, review) => {
+            const textReview = $(review).find('.review-text').text();
+            console.log(textReview);
+        })
     })
-})
+    readline.close();
+});
